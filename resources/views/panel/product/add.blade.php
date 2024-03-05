ff02aeddc059a9ccab179af5e9215169e77798f7
mehrabkb <mkordbacheh75@gmail.com>
2024-03-05 13:36:09 +0330

    add product pages blade in project

---
+++ resources/views/panel/product/add.blade.php
@@ -0,0 +1,36 @@
+@extends('panel.master')
+@section('title' , 'افزودن')
+
+@section('content')
+    <section class="content">
+        <div class="row">
+            <section class="col-lg-12 col-md-12">
+                <div class="box box-info">
+                    <div class="box-header">
+                        <i class="fa fa-info-circle"></i>
+                        <h3 class="box-title">قالب AdminLTE فارسی ورژن 2.4.0</h3>
+                        <!-- tools box -->
+                        <div class="pull-left box-tools">
+                            <button type="button" class="btn bg-info btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                        </div>
+                        <!-- /. tools -->
+                    </div>
+                    <div class="box-body">
+                        <h5>این قالب به صورت html هست و برای بخش کنترل پنل و مدیریت سایت های مختلف مناسب می باشد.</h5>
+                        <h5>نسخه اصلی این قالب AdminLTE می باشد که توسط <a href="https://adminlte.io">Almsaeed Studio</a> به صورت رایگان قرار داده شده است.</h5>
+                        <br><h5><b>ویژگی های قالب و تفاوت های آن با قالب اصلی:</b></h5>
+                        <h5>۱- قالب به صورت کامل و حرفه ای فارسی و راست چین شده.</h5>
+                        <h5>۲- انتخاب تاریخ به صورت شمسی یا دیتا پیکر توسط کتاب خانه باباخانی اضافه شده.</h5>
+                        <h5>۳- ویرایشگر CK Editor فارسی و راست چین شده.</h5>
+                        <h5>۴- ویرایشگر TinyMCE فارسی و راست چین شده و به قالب اضافه شده.</h5>
+                        <h5>۵- همچنین فونت فارسی برای خوانایی بیشتر حروف و اعداد فارسی به قالب افزوده شد.</h5>
+                        <br>
+                        <h6><b>قالب توسط علیرضا حسینی زاده و به صورت رایگان منتشر شده است.</b></h6>
+                        <h6><b>برای دانلود قالب و دریافت آپدیت های آن به این <a href="https://github.com/hosseinizadeh/AdminLTE_Persian">وب سایت</a> مراجعه کنید.</b></h6>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
+@endsection
